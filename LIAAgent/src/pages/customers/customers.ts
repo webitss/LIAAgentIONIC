1e16552151741d6a6516486a618abda6ba55b8aa
chayakwebit <avikchaya@gmail.com>
2017-12-06 15:44:53 +0200

    בניית מסך לקוחות לא גמור

--- LIAAgent/src/pages/customers/customers.ts
+++ LIAAgent/src/pages/customers/customers.ts
@@ -19,19 +19,23 @@ export class CustomersPage {
   valueButton:string;
   allowDetails:boolean;
   TabsEnum: typeof TabsEnum = TabsEnum;
+  items1 = [];
+
+
   constructor(public navCtrl: NavController, public navParams: NavParams,public service:LiaService) {
     this.valueButton="לקוח חדש";
     service.nowComponent="לקוחות";
     this.allowDetails=false;
+    this.items1 = [1,2,3,4,5,6,7,8,9,10,10];
   }
-  
+
   customerClicked(){
     this.allowDetails=!this.allowDetails;
     this.valueButton=this.valueButton==="לקוח חדש"?"עבור לסל":"לקוח חדש";
     }
 
     routeToCart(){
-      this.navCtrl.parent.select(this.TabsEnum.cart); 
+      this.navCtrl.parent.select(this.TabsEnum.cart);
     }
 
     ionViewWillEnter()
